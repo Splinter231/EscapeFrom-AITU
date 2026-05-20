31b19d58cf8815d1712b42e3e354f7285d94f643
Sergey Chepurnenko <serega.chepur333@mail.ru>
2026-05-20 15:50:27 +0500

    Add player health system

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    private Animator animator;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+
+        if (HUDManager.Instance != null)
+        {
+            HUDManager.Instance.UpdateHealth(currentHealth, maxHealth);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        if (HUDManager.Instance != null)
+        {
+            HUDManager.Instance.UpdateHealth(currentHealth, maxHealth);
+        }
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        if (HUDManager.Instance != null)
+        {
+            HUDManager.Instance.UpdateHealth(currentHealth, maxHealth);
+        }
+    }
+
+    void Die()
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger("Lose");
+        }
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.LoseGame();
+        }
+    }
+}
